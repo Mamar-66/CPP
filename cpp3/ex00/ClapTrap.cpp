bd899a251cde8f936cedbd2067e34798d236da67
mamar66 <omarfelk66@gmail.com>
2024-09-11 16:50:08 +0200

    cpp3 ex03 ok

--- cpp3/ex00/ClapTrap.cpp
+++ cpp3/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: omfelk <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 15:39:33 by omfelk            #+#    #+#             */
-/*   Updated: 2024/09/10 17:17:07 by omfelk           ###   ########.fr       */
+/*   Updated: 2024/09/11 16:46:40 by omfelk           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,29 +17,29 @@
 ----------------------------------------------------*/
 
 ClapTrap::ClapTrap(void) :
-	_name("Default"),
+	_name(GREEN "Default" RESET),
 	_hit(10), _energy(10), _attack(0)
 {
-	std::cout << "Constructor >> " GREEN "default" RESET << " << called" << std::endl;
+	std::cout << RED "Constructor >> " RESET << _name << " << called" << std::endl;
 }
 
 ClapTrap::ClapTrap(std::string name) :
 	_name(name),
 	_hit(10), _energy(10), _attack(0)		
 {
-	std::cout << "Constructor >> " GREEN << name << RESET " << called" << std::endl;
+	std::cout << RED "Constructor >> " RESET << name << " << called" << std::endl;
 }
 
 ClapTrap::ClapTrap(ClapTrap& claptrap):
 	_name(claptrap._name),
 	_hit(claptrap._hit), _energy(claptrap._energy), _attack(claptrap._attack)	
 {
-		std::cout << "Constructor copy >> " GREEN << claptrap._name << " << called" << std::endl;
+		std::cout << RED "Constructor copy >> " RESET GREEN << claptrap._name << " << called" << std::endl;
 }
 
 ClapTrap::~ClapTrap(void)
 {
-	std::cout << "Destructor called" << std::endl;
+	std::cout << RED "Destructor >> " RESET 	<< this->_name << " << called" << std::endl;
 }
 
 /*----------------------------------------------------
@@ -48,18 +48,46 @@ ClapTrap::~ClapTrap(void)
 
 void	ClapTrap::attack(const std::string& target)
 {
-	(void)target;
-	std::cout << "void" << std::endl;
+	std::cout << "ClapTrap " << this->_name;
+	if (this->_energy > 0 && this->_hit > 0)
+	{
+		this->_energy--;
+		std::cout << " attacks " ORANGE << target << RESET " , caussing " ORANGE << this->_attack << RESET " points of damage ";
+		std::cout << this->_name << " has " ORANGE << this->_energy << RESET " energy" << std::endl;
+	}
+	else
+		std::cout << " for attack not enought energy points or dead" << std::endl;
 }
 
 void	ClapTrap::beRepaired(unsigned int amount)
 {
-	(void)amount;
-	std::cout << "void" << std::endl;
+	std::cout << "ClapTrap " << this->_name;
+	if (this->_energy > 0 && this->_hit > 0)
+	{
+		this->_hit += amount;
+		this->_energy--;
+		std::cout << " take " ORANGE << amount << RESET " hit points " << this->_name << " now have " ORANGE << this->_hit << RESET << std::endl;
+	}
+	else
+		std::cout << " for repaired not enought energy points or dead" << std::endl;
 }
 
 void	ClapTrap::takeDamage(unsigned int amount)
 {
-	(void)amount;
-	std::cout << "void" << std::endl;
+	std::cout << "ClapTrap " << this->_name;
+	if (this->_hit <= 0)
+		std::cout << " is already dead" << std::endl;
+	else if (amount >= (unsigned int)this->_hit)
+	{
+		std::cout << " have " ORANGE<< this->_hit <<RESET " pv, " << this->_name;
+		std::cout << " take " ORANGE << amount << RESET " damage and dead" << std::endl;
+		this->_hit = 0;
+	}
+	else
+	{
+		this->_hit -= amount;
+		std::cout << " take " ORANGE << amount << RESET " damage ";
+		std::cout << this->_name << " has " ORANGE << this->_hit << RESET " pv " << std::endl;
+
+	}
 }
--- cpp3/ex00/main.cpp
+++ cpp3/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: omfelk <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 15:16:24 by omfelk            #+#    #+#             */
-/*   Updated: 2024/09/10 17:16:04 by omfelk           ###   ########.fr       */
+/*   Updated: 2024/09/11 16:48:51 by omfelk           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,35 @@
 
 int	main(void)
 {
-	ClapTrap playeur1("playeur1");
-	ClapTrap playeur2;
+	std::cout << std::endl << std::string(70, '-') << std::endl << std::endl;
+	{
+		ClapTrap playeur1(GREEN "Hero" RESET);
 
-	playeur1.attack("playeur1");
-	playeur2.attack("") ;
-	
+		std::cout << RED "\nTEST ENERGY\n" RESET << std::endl;
+		for (int i = 0; i <= 10; i++)
+			playeur1.attack("bobo");
+		playeur1.beRepaired(2);
+		std::cout << "\n";
+	}
+
+	std::cout << std::endl << std::string(70, '-') << std::endl << std::endl;
+
+	{
+		ClapTrap playeur1(GREEN "Hero" RESET);
+
+		std::cout << RED "\nTEST TAKE DAMAGE\n" RESET << std::endl;
+
+		for (int i = 0; i < 4; i++)
+			playeur1.takeDamage(2);
+		for (int i = 0; i < 12; i++)
+			playeur1.beRepaired(2);
+		playeur1.takeDamage(15);
+		playeur1.takeDamage(15);
+		playeur1.takeDamage(15);
+		playeur1.attack("bobo");
+		playeur1.beRepaired(2);
+		std::cout << "\n";
+	}
+	std::cout << std::endl << std::string(70, '-') << std::endl << std::endl;
 	return 1;
 }
