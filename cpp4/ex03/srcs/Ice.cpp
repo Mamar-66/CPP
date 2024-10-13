75674968b5ba13afe3201cda390f26e3c60b414d
mamar66 <omarfelk66@gmail.com>
2024-10-13 18:39:16 +0200

    cpp4 ex02 ice en cours

--- cpp4/ex03/srcs/Ice.cpp
+++ cpp4/ex03/srcs/Ice.cpp
@@ -6,7 +6,7 @@
 /*   By: omfelk <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 10:06:36 by omfelk            #+#    #+#             */
-/*   Updated: 2024/10/13 16:17:37 by omfelk           ###   ########.fr       */
+/*   Updated: 2024/10/13 18:38:50 by omfelk           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,15 @@ Ice::Ice() : AMateria("ice")
 Ice::Ice(const Ice &ice) : AMateria(ice)
 {
 	std::cout << ORANGE "Copy Ice Constructor >> " RED << this->_type << ORANGE " << called" RESET << std::endl;
+}
+
+Ice::~Ice()
+{
+	std::cout << ORANGE " Destructor Ice >> " RED << this->_type << ORANGE " << called" RESET << std::endl;
+}
+
+Ice& Ice::operator=(const Ice& ice)
+{
+	this->AMateria::operator=(ice);
+	return *this;
 }
