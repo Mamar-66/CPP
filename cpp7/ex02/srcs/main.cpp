27248e6be2948c7643a671aa188232c5bc40c503
mamar66 <omarfelk66@gmail.com>
2024-10-30 16:37:22 +0100

    cpp7 ex2 finish corection affichage

--- cpp7/ex02/srcs/main.cpp
+++ cpp7/ex02/srcs/main.cpp
@@ -6,11 +6,12 @@
 /*   By: omfelk <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 23:50:33 by omfelk            #+#    #+#             */
-/*   Updated: 2024/10/28 11:58:51 by omfelk           ###   ########.fr       */
+/*   Updated: 2024/10/30 16:35:12 by omfelk           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
+#include <iomanip>
 
 int	main()
 {
@@ -51,13 +52,13 @@ std::cout << BLUE << std::string(50, '-') << RESET << std::endl;
 std::cout << BLUE << std::string(50, '-') << RESET << std::endl;
 	{
 		std::cout << GREEN << "FLOAT" << RESET << std::endl;
-		Array<float>	tab1(5);
+		Array<float>	tab1(5.0f);
 
-		for (float i = 0; i < tab1.size(); i++)
+		for (float i = 0.0f; i < tab1.size(); i++)
 		{
 			try
 			{
-				std::cout << "tab : " << tab1[i] << std::endl;
+				std::cout << "tab : " << std::fixed << std::setprecision(2) << tab1[i] << std::endl;
 			}
 			catch(const std::exception& e)
 			{
@@ -69,11 +70,11 @@ std::cout << BLUE << std::string(50, '-') << RESET << std::endl;
 		{
 			tab1[i] = i;
 		}
-		for (float i = -2; i < 7; i++)
+		for (float i = -2.0f; i < 7; i++)
 		{
 			try
 			{
-				std::cout << "tab : " << tab1[i] << std::endl;
+				std::cout << "tab : " << std::fixed << std::setprecision(2) << tab1[i] << std::endl;
 			}
 			catch(const std::exception& e)
 			{
@@ -87,11 +88,11 @@ std::cout << BLUE << std::string(50, '-') << RESET << std::endl;
 		std::cout << GREEN << "DOUBLE" << RESET << std::endl;
 		Array<float>	tab1(5);
 
-		for (double i = 0; i < tab1.size(); i++)
+		for (double i = 0.0; i < tab1.size(); i++)
 		{
 			try
 			{
-				std::cout << "tab : " << tab1[i] << std::endl;
+				std::cout << "tab : " << std::fixed << std::setprecision(2) << tab1[i] << std::endl;
 			}
 			catch(const std::exception& e)
 			{
@@ -107,7 +108,7 @@ std::cout << BLUE << std::string(50, '-') << RESET << std::endl;
 		{
 			try
 			{
-				std::cout << "tab : " << tab1[i] << std::endl;
+				std::cout << "tab : " << std::fixed << std::setprecision(2) << tab1[i] << std::endl;
 			}
 			catch(const std::exception& e)
 			{
