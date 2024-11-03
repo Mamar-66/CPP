5e85df5235b2abaa30bd5abba261d697319619ac
mamar66 <omarfelk66@gmail.com>
2024-11-03 17:54:53 +0100

    cpp8 ex01 continue

--- cpp8/ex01/includes/Span.hpp
+++ cpp8/ex01/includes/Span.hpp
@@ -6,7 +6,7 @@
 /*   By: omfelk <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 16:02:45 by omfelk            #+#    #+#             */
-/*   Updated: 2024/11/02 17:00:47 by omfelk           ###   ########.fr       */
+/*   Updated: 2024/11/03 17:46:22 by omfelk           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,50 @@
 	#define SPAN_HPP
 
 	#include <iostream>
+	#include <vector>
+
+	#define RED     "\033[31m"
+	#define ORANGE  "\033[38;5;208m"
+	#define GREEN   "\033[32m"
+	#define BLUE    "\033[34m"
+	#define RESET   "\033[0m"
+	#define BOLD    "\033[1m"
+	#define UNDERLINE "\033[4m"
 
 	class Span
 	{
+		private :
+			unsigned int					_maxNb;
+			unsigned int					_storyteller;
+			std::vector<unsigned int>		_array;
+
 		public :
 			Span();
-			Span(unsigned int number);
+			Span(const unsigned int maxnb);
 			Span(const Span &other);
 			~Span();
 
 			Span&	operator=(const Span &other);
 
-			void addNumber(int number);
+			void				addNumber(unsigned int number);
+			unsigned int		shortestSpan();
+			unsigned int		longestSpan();
 
+			class ExceptionMaxNb : public std::exception
+			{
+				public :
+					const char *what() const throw();
+			};
+			class ExceptionNoNb : public std::exception
+			{
+				public :
+					const char *what() const throw();
+			};
+			class ExceptionOnNb : public std::exception
+			{
+				public :
+					const char *what() const throw();
+			};
 	};
 
 #endif
--- cpp8/ex01/srcs/Span.cpp
+++ cpp8/ex01/srcs/Span.cpp
@@ -6,9 +6,70 @@
 /*   By: omfelk <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 16:02:26 by omfelk            #+#    #+#             */
-/*   Updated: 2024/11/02 16:08:40 by omfelk           ###   ########.fr       */
+/*   Updated: 2024/11/03 17:51:53 by omfelk           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Span.hpp"
 
+Span::Span() : _maxNb(0), _storyteller(0)
+{
+	std::cout << ORANGE "Constructeur >> " RED << "default" << " << called" RESET << std::endl;
+}
+
+Span::Span(const unsigned int maxnb) : _maxNb(maxnb), _storyteller(0)
+{
+	std::cout << ORANGE "Constructeur called" RESET << std::endl;
+}
+
+Span::Span(const Span &other) : _maxNb(other._maxNb), _storyteller(other._storyteller)
+{
+	std::cout << ORANGE "Constructeur >> " RED << "Copy" << " << called" RESET << std::endl;
+}
+
+Span::~Span()
+{
+	std::cout << ORANGE "Destructor called" RESET << std::endl;
+}
+
+Span&	Span::operator=(const Span &other)
+{
+	if (this != &other)
+	{
+		this->_maxNb = other._maxNb;
+		this->_storyteller = other._storyteller;
+	}
+	return *this;
+}
+
+/*----------------------------------------------------------------------*/
+/*							FUCTION MEMBER								*/
+/*----------------------------------------------------------------------*/
+
+void	Span::addNumber(unsigned int number)
+{
+	if (this->_storyteller >= this->_maxNb)
+		throw Span::ExceptionMaxNb();
+
+	this->_array.push_back(number);
+	this->_storyteller++;
+}
+
+/*----------------------------------------------------------------------*/
+/*							EXEPTION									*/
+/*----------------------------------------------------------------------*/
+
+const char * Span::ExceptionMaxNb::what() const throw()
+{
+	return (RED "error max size nb" RESET);
+}
+
+const char * Span::ExceptionNoNb::what() const throw()
+{
+	return (RED "error no number" RESET);
+}
+
+const char * Span::ExceptionOnNb::what() const throw()
+{
+	return (RED "error one nb" RESET);
+}
