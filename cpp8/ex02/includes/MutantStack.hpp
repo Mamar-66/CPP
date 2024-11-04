533fa39c64bdfc3582684c4df428c397fd5a2170
Omar Felk <omfelk@c3r5p9.42perpignan.fr>
2024-11-04 16:49:33 +0100

    spp8 ex02 start

---
+++ cpp8/ex02/includes/MutantStack.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omfelk <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 16:32:43 by omfelk            #+#    #+#             */
+/*   Updated: 2024/11/04 16:39:18 by omfelk           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+	#define MUTANTSTACK_HPP
+
+	#include <iostream>
+
+	class	MutantStack
+	{
+
+	};
+
+	#include "../includes/MutantStack.hpp"
+
+#endif
