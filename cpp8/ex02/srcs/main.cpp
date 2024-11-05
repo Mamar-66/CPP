6ecfabdd76790f17d528f6cc90d1b1e9365226d2
Omar Felk <omfelk@c3r5p9.42perpignan.fr>
2024-11-05 16:08:04 +0100

    modif test

--- cpp8/ex02/srcs/main.cpp
+++ cpp8/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: omfelk <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 16:32:25 by omfelk            #+#    #+#             */
-/*   Updated: 2024/11/05 12:33:13 by omfelk           ###   ########.fr       */
+/*   Updated: 2024/11/05 16:07:14 by omfelk           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,62 +17,52 @@
 int main()
 {
 	{
-		MutantStack<int> mstack;
+		MutantStack<int>	mstack;
+		std::list<int>		test;
 
 		mstack.push(5);
 		mstack.push(17);
 
-		std::cout << mstack.top() << std::endl;
+		test.push_back(5);
+		test.push_back(17);
+
+		std::cout << mstack.top() << " : ";
+		std::cout << test.back() << std::endl;
 
 		mstack.pop();
+		test.pop_back();
 
-		std::cout << mstack.size() << std::endl;
+
+		std::cout << mstack.size() << " : ";
+		std::cout << test.size() << std::endl;
 
 		mstack.push(3);
 		mstack.push(5);
 		mstack.push(737);
 		mstack.push(0);
 
+		test.push_back(3);
+		test.push_back(5);
+		test.push_back(737);
+		test.push_back(0);
+
 		MutantStack<int>::iterator it = mstack.begin();
 		MutantStack<int>::iterator ite = mstack.end();
 
-		++it;
-		--it;
-
-		while (it != ite)
-		{
-			std::cout << *it << std::endl;
-			++it;
-		}
-	}
-	std::cout << std::string(50, '-') << std::endl;
-	{
-		std::list<int> mstack;
-
-		mstack.push_back(5);
-		mstack.push_back(17);
-
-		std::cout << mstack.back() << std::endl;
-
-		mstack.pop_back();
-
-		std::cout << mstack.size() << std::endl;
-
-		mstack.push_back(3);
-		mstack.push_back(5);
-		mstack.push_back(737);
-		mstack.push_back(0);
-
-		std::list<int>::iterator it = mstack.begin();
-		std::list<int>::iterator ite = mstack.end();
+		std::list<int>::iterator it_test = test.begin();
+		std::list<int>::iterator ite_test = test.end();
 
 		++it;
+		++it_test;
 		--it;
+		--it_test;
 
-		while (it != ite)
+		while (it != ite && it_test != ite_test)
 		{
-			std::cout << *it << std::endl;
+			std::cout << *it << " : ";
+			std::cout << *it_test << std::endl;
 			++it;
+			++it_test;
 		}
 	}
 }
